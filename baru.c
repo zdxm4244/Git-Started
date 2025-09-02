27678b05998fe7b987d4844e9333e4a34287f457
zdxm4244 <skwcarlos@gmail.com>
2025-09-02 16:36:10 +0700

    Add baru.c

---
+++ baru.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+  int A;
+  int B;
+  printf("Enter two sets of numbers that you want to substract:\n");
+  scanf("%d %d", &A, &B);
+  printf("The result is: %d\n", A - B);
+  return 0;
+  
+}
